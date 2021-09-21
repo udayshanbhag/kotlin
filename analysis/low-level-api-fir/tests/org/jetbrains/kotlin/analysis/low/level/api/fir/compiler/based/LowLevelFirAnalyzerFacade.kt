75eea995c73dc999e754a0a8b49746f4a3d7101d
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-09-21 09:23:16 +0200

    LL API: add check to the compiler based tests to ensure diagnostic params validity for IDE

--- analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/compiler/based/LowLevelFirAnalyzerFacade.kt
+++ analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/compiler/based/LowLevelFirAnalyzerFacade.kt
@@ -5,21 +5,22 @@
 
 package org.jetbrains.kotlin.analysis.low.level.api.fir.compiler.based
 
+import org.jetbrains.kotlin.analysis.low.level.api.fir.api.DiagnosticCheckerFilter
+import org.jetbrains.kotlin.analysis.low.level.api.fir.api.FirModuleResolveState
+import org.jetbrains.kotlin.analysis.low.level.api.fir.api.collectDiagnosticsForFile
+import org.jetbrains.kotlin.analysis.low.level.api.fir.api.resolvedFirToPhase
 import org.jetbrains.kotlin.fir.analysis.AbstractFirAnalyzerFacade
-import org.jetbrains.kotlin.fir.analysis.diagnostics.FirDiagnostic
+import org.jetbrains.kotlin.fir.analysis.diagnostics.*
 import org.jetbrains.kotlin.fir.backend.Fir2IrResult
 import org.jetbrains.kotlin.fir.declarations.FirFile
 import org.jetbrains.kotlin.fir.declarations.FirResolvePhase
 import org.jetbrains.kotlin.fir.psi
 import org.jetbrains.kotlin.fir.resolve.ScopeSession
 import org.jetbrains.kotlin.fir.resolve.transformers.FirSealedClassInheritorsProcessor
-import org.jetbrains.kotlin.analysis.low.level.api.fir.api.DiagnosticCheckerFilter
-import org.jetbrains.kotlin.analysis.low.level.api.fir.api.FirModuleResolveState
-import org.jetbrains.kotlin.analysis.low.level.api.fir.api.collectDiagnosticsForFile
-import org.jetbrains.kotlin.analysis.low.level.api.fir.api.resolvedFirToPhase
+import org.jetbrains.kotlin.fir.types.ConeKotlinType
+import org.jetbrains.kotlin.fir.types.ConeTypeVariableType
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi2ir.generators.GeneratorExtensions
-import org.jetbrains.kotlin.test.directives.FirDiagnosticsDirectives
 import org.jetbrains.kotlin.test.model.TestFile
 
 class LowLevelFirAnalyzerFacade(
@@ -33,11 +34,14 @@ class LowLevelFirAnalyzerFacade(
         findSealedInheritors()
         return allFirFiles.values.associateWith { firFile ->
             val ktFile = firFile.psi as KtFile
-            val diagnostics = ktFile.collectDiagnosticsForFile(resolveState, diagnosticCheckerFilter)
+
             @Suppress("UNCHECKED_CAST")
-            diagnostics.toList() as List<FirDiagnostic>
+            val diagnostics = ktFile.collectDiagnosticsForFile(resolveState, diagnosticCheckerFilter).toList() as List<FirDiagnostic>
+            DiagnosticChecker.checkDiagnosticsIsSuitableForFirIde(diagnostics)
+            diagnostics
         }
     }
+
 
     private fun findSealedInheritors() {
         allFirFiles.values.forEach {
@@ -49,6 +53,45 @@ class LowLevelFirAnalyzerFacade(
 
     override fun runResolution(): List<FirFile> = shouldNotBeCalled()
     override fun convertToIr(extensions: GeneratorExtensions): Fir2IrResult = shouldNotBeCalled()
+}
+
+private object DiagnosticChecker {
+    fun checkDiagnosticsIsSuitableForFirIde(diagnostics: List<FirDiagnostic>) {
+        for (diagnostic in diagnostics) {
+            checkDiagnosticIsSuitableForFirIde(diagnostic)
+        }
+    }
+
+    private fun checkDiagnosticIsSuitableForFirIde(diagnostic: FirDiagnostic) {
+        val parameters = diagnostic.allParameters()
+        for (parameter in parameters) {
+            checkDiagnosticParameter(diagnostic, parameter)
+        }
+    }
+
+    private fun checkDiagnosticParameter(diagnostic: FirDiagnostic, parameter: Any?) {
+        if (parameter is ConeTypeVariableType) {
+            val rendered = FirDefaultErrorMessages.getRendererForDiagnostic(diagnostic).render(diagnostic)
+            error(
+                "ConeTypeVariableType should not be exposed from diagnostic. " +
+                        "But it was for ${diagnostic.factoryName} $rendered"
+            )
+        }
+        if (parameter is ConeKotlinType) {
+            for (typeArgument in parameter.typeArguments) {
+                checkDiagnosticParameter(diagnostic, typeArgument)
+            }
+        }
+    }
+
+    private fun FirDiagnostic.allParameters(): List<Any?> = when (this) {
+        is FirPsiDiagnosticWithParameters1<*> -> listOf(a)
+        is FirPsiDiagnosticWithParameters2<*, *> -> listOf(a, b)
+        is FirPsiDiagnosticWithParameters3<*, *, *> -> listOf(a, b, c)
+        is FirPsiDiagnosticWithParameters4<*, *, *, *> -> listOf(a, b, c, d)
+        is FirPsiSimpleDiagnostic -> emptyList()
+        else -> error("Unexpected diagnostic $this")
+    }
 }
 
 private fun shouldNotBeCalled(): Nothing = error("Should not be called for LL test")
