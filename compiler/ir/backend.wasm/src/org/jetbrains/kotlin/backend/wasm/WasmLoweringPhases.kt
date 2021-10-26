7cf35bb52609bbd80af7505f57a06cb34b3edf0c
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2021-10-26 19:26:17 +0200

    [WASM] Implementation of Class references

--- compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/WasmLoweringPhases.kt
+++ compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/WasmLoweringPhases.kt
@@ -404,6 +404,12 @@ private val staticMembersLoweringPhase = makeWasmModulePhase(
     description = "Move static member declarations to top-level"
 )
 
+private val classReferenceLoweringPhase = makeWasmModulePhase(
+    ::ClassReferenceLowering,
+    name = "ClassReferenceLowering",
+    description = "Handle class references"
+)
+
 private val wasmVarargExpressionLoweringPhase = makeWasmModulePhase(
     ::WasmVarargExpressionLowering,
     name = "WasmVarargExpressionLowering",
@@ -512,6 +518,8 @@ val wasmPhases = NamedCompilerPhase(
             wrapInlineDeclarationsWithReifiedTypeParametersPhase then
 
             functionInliningPhase then
+            removeInlineDeclarationsWithReifiedTypeParametersLoweringPhase then
+
             lateinitNullableFieldsPhase then
             lateinitDeclarationLoweringPhase then
             lateinitUsageLoweringPhase then
@@ -562,11 +570,10 @@ val wasmPhases = NamedCompilerPhase(
             defaultArgumentPatchOverridesPhase then
             defaultParameterInjectorPhase then
             defaultParameterCleanerPhase then
-            removeInlineDeclarationsWithReifiedTypeParametersLoweringPhase then
 
 //            TODO:
 //            multipleCatchesLoweringPhase then
-//            classReferenceLoweringPhase then
+            classReferenceLoweringPhase then
 
             wasmVarargExpressionLoweringPhase then
             inlineClassDeclarationLoweringPhase then
