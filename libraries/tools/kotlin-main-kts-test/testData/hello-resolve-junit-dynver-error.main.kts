9f7c791cf55aa746b83b962425d002b36ff3bd56
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2021-11-19 00:37:12 +0300

    -- revert

--- libraries/tools/kotlin-main-kts-test/testData/hello-resolve-junit-dynver-error.main.kts
+++ libraries/tools/kotlin-main-kts-test/testData/hello-resolve-junit-dynver-error.main.kts
@@ -1,5 +1,5 @@
-
-@file:DependsOn("junit:junit:(4.12,4.13.2]")
+// // org.junit.Assert.assertThrows was added in 4.13, compilation should fail
+@file:DependsOn("junit:junit:(4.11,4.12]")
 
 org.junit.Assert.assertThrows(NullPointerException::class.java) {
     throw null!!
--- libraries/tools/kotlin-main-kts-test/testData/hello-resolve-junit-dynver.main.kts
+++ libraries/tools/kotlin-main-kts-test/testData/hello-resolve-junit-dynver.main.kts
@@ -1,5 +1,5 @@
-
-@file:DependsOn("junit:junit:(4.13.2,5.0)")
+// org.junit.Assert.assertThrows was added in 4.13, compilation should be fine
+@file:DependsOn("junit:junit:(4.12,5.0)")
 
 org.junit.Assert.assertThrows(NullPointerException::class.java) {
     throw null!!
