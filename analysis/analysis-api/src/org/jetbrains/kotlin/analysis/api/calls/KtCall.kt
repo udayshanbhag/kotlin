3a0ee4ed1940b7c34d442b2c29ff7ea566285160
Tianyu Geng <tgeng@google.com>
2021-11-18 17:56:24 -0800

    Analysis API: rework data model of resolve call API

--- analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/calls/KtCall.kt
+++ analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/calls/KtCall.kt
@@ -6,172 +6,311 @@
 package org.jetbrains.kotlin.analysis.api.calls
 
 import org.jetbrains.kotlin.analysis.api.ValidityTokenOwner
-import org.jetbrains.kotlin.analysis.api.diagnostics.KtDiagnostic
-import org.jetbrains.kotlin.analysis.api.symbols.KtFunctionLikeSymbol
-import org.jetbrains.kotlin.analysis.api.symbols.KtValueParameterSymbol
-import org.jetbrains.kotlin.analysis.api.symbols.KtVariableLikeSymbol
+import org.jetbrains.kotlin.analysis.api.symbols.*
 import org.jetbrains.kotlin.analysis.api.tokens.ValidityToken
 import org.jetbrains.kotlin.analysis.api.types.KtSubstitutor
+import org.jetbrains.kotlin.analysis.api.types.KtType
 import org.jetbrains.kotlin.analysis.api.withValidityAssertion
 import org.jetbrains.kotlin.psi.KtExpression
 
 /**
- * Represents direct or indirect (via invoke) function call from Kotlin code
+ * Call information at call site.
  */
-public sealed class KtCall : ValidityTokenOwner {
-    public abstract val isErrorCall: Boolean
-    public abstract val argumentMapping: LinkedHashMap<KtExpression, KtValueParameterSymbol>
-    public abstract val targetFunction: KtCallTarget
+public sealed class KtCallInfo : ValidityTokenOwner
+
+/**
+ * Successfully resolved call.
+ */
+public abstract class KtSuccessCallInfo : KtCallInfo() {
+    public abstract val call: KtCall
+}
+
+/**
+ * Call that contains errors.
+ */
+public abstract class KtErrorCallInfo : KtCallInfo() {
+    public abstract val candidateCalls: List<KtCall>
+}
+
+/**
+ * A call to a function, a simple/compound access to a property, or a simple/compound access through `get` and `set` convention.
+ */
+public sealed class KtCall : ValidityTokenOwner
+
+/**
+ * A callable symbol bound with receivers.
+ */
+// TODO: make this not abstract
+public abstract class KtSymbolAndReceiver<out S : KtCallableSymbol> : ValidityTokenOwner {
+    /**
+     * The function or variable (property) declaration.
+     */
+    public abstract val symbol: S
+
+    /**
+     * The dispatch receiver for this symbol access. Dispatch receiver is available if the symbol is declared inside a class or object.
+     */
+    public abstract val dispatchReceiver: KtReceiverValue?
+
+    /**
+     * The extension receiver for this symbol access. Extension receiver is available if the symbol is declared with an extension receiver.
+     */
+    public abstract val extensionReceiver: KtReceiverValue?
+
+    /**
+     * The substitutor to substitute type parameters in referenced symbol with types at the call site.
+     */
     public abstract val substitutor: KtSubstitutor
 }
 
 /**
- * Call using `()` of some variable of functional type, e.g.,
+ * A call to a function, or a simple/compound access to a property.
+ */
+public sealed class KtCallableMemberCall : KtCall() {
+    public abstract val symbolAndReceiver: KtSymbolAndReceiver<KtCallableSymbol>
+}
+
+/**
+ * A call to a function.
+ */
+// TODO: make this not abstract
+public abstract class KtFunctionCall : KtCallableMemberCall() {
+    /**
+     * The function and receivers for this call.
+     */
+    abstract override val symbolAndReceiver: KtSymbolAndReceiver<KtFunctionLikeSymbol>
+
+    /**
+     * The mapping from argument to parameter declaration. In case of vararg parameters, multiple arguments may be mapped to the same
+     * `KtValueParameterSymbol`.
+     */
+    public abstract val argumentMapping: LinkedHashMap<KtExpression, KtValueParameterSymbol>
+
+    /**
+     * Whether this function call is an implicit invoke call on a value that has an `invoke` member function. See
+     * https://kotlinlang.org/docs/operator-overloading.html#invoke-operator for more details.
+     */
+    public abstract val isImplicitInvoke: Boolean
+}
+
+/**
+ * An access to variables (including properties).
+ */
+public sealed class KtVariableAccessCall : KtCallableMemberCall() {
+    /**
+     * The variable/property and receivers for this access.
+     */
+    abstract override val symbolAndReceiver: KtSymbolAndReceiver<KtVariableLikeSymbol>
+}
+
+/**
+ * A simple read or write to a variable or property.
+ */
+// TODO: make this not abstract
+public abstract class KtSimpleVariableAccessCall : KtVariableAccessCall() {
+
+    /**
+     * The type of access to this property.
+     */
+    public abstract val simpleAccess: KtSimpleVariableAccess
+}
+
+public sealed class KtSimpleVariableAccess {
+    public object Read : KtSimpleVariableAccess()
+
+    // TODO: make this not abstract
+    public abstract class Write(public val value: KtExpression) : KtSimpleVariableAccess(), ValidityTokenOwner
+
+}
+
+public interface KtCompoundAccessCall {
+    /**
+     * The type of this compound access.
+     */
+    public val compoundAccess: KtCompoundAccess
+}
+
+/**
+ * A compound access of a mutable variable.  For example
+ * ```
+ * fun test() {
+ *   var i = 0
+ *   i += 1
+ *   // symbolAndReceiver: {
+ *   //   symbol: `i`
+ *   //   dispatchReceiver: null
+ *   //   extensionReceiver: null
+ *   // }
+ *   // accessType: OpAssign {
+ *   //   kind: PLUS
+ *   //   operand: 1
+ *   //   operationSymbol: Int.plus()
+ *   // }
  *
- * fun x(f: () -> Int) {
- *    f() // functional type call
+ *   i++
+ *   // symbolAndReceiver: {
+ *   //   symbol: `i`
+ *   //   dispatchReceiver: null
+ *   //   extensionReceiver: null
+ *   // }
+ *   // accessType: IncDec {
+ *   //   kind: INC
+ *   //   precedence: POSTFIX
+ *   //   operationSymbol: Int.inc()
+ *   // }
  * }
+ * ```
+ * Note that if the variable has a `<op>Assign` member, then it's represented as a simple `KtFunctionCall`. For example,
+ * ```
+ * fun test(m: MutableList<String>) {
+ *   m += "a" // A simple `KtFunctionCall` to `MutableList.plusAssign`, not a `KtVariableAccessCall`. However, the dispatch receiver of this
+ *            // call, `m`, is a simple read access represented as a `KtVariableAccessCall`
+ * }
+ * ```
  */
-public class KtFunctionalTypeVariableCall(
-    private val _target: KtVariableLikeSymbol,
-    private val _argumentMapping: LinkedHashMap<KtExpression, KtValueParameterSymbol>,
-    private val _targetFunction: KtCallTarget,
-    private val _substitutor: KtSubstitutor,
-    override val token: ValidityToken
-) : KtCall() {
-    public val target: KtVariableLikeSymbol get() = withValidityAssertion { _target }
-    override val isErrorCall: Boolean get() = withValidityAssertion { false }
-    override val argumentMapping: LinkedHashMap<KtExpression, KtValueParameterSymbol>
-        get() = withValidityAssertion { _argumentMapping }
-    override val targetFunction: KtCallTarget
-        get() = withValidityAssertion { _targetFunction }
-    override val substitutor: KtSubstitutor
-        get() = withValidityAssertion { _substitutor }
-}
+// TODO: make this not abstract
+public abstract class KtCompoundVariableAccessCall : KtVariableAccessCall(), KtCompoundAccessCall
 
 /**
- * Direct or indirect call of function declared by user
- */
-public sealed class KtDeclaredFunctionCall : KtCall() {
-    override val isErrorCall: Boolean
-        get() = withValidityAssertion { targetFunction is KtErrorCallTarget }
-}
-
-/**
- * Call using () on variable on some non-functional type, considers that `invoke` function is declared somewhere
+ * A compound access using the array access convention. For example,
+ * ```
+ * fun test(m: MutableMap<String, String>) {
+ *   m["a"] += "b"
+ *   // indexArguments: ["a"]
+ *   // getSymbolAndReceiver: {
+ *   //   symbol: MutableMap.get()
+ *   //   dispatchReceiver: `m`
+ *   //   extensionReceiver: null
+ *   // }
+ *   // setSymbolAndReceiver: {
+ *   //   symbol: MutableMap.set()
+ *   //   dispatchReceiver: `m`
+ *   //   extensionReceiver: null
+ *   // }
+ *   // accessType: OpAssign {
+ *   //   kind: PLUS
+ *   //   operand: "b"
+ *   //   operationSymbol: String?.plus()
+ *   // }
+ * }
+ * ```
+ * Such a call always involve both calls to `get` and `set` functions. With the example above, a call to `String?.plus` is sandwiched
+ * between `get` and `set` call to compute the new value passed to `set`.
  *
- * fun x(y: Int) {
- *    y() // variable with invoke function call
+ * Note that simple access using the array access convention is not captured by this class. For example, assuming `ThrowingMap` throws
+ * in case of absent key instead of returning `null`,
+ * ```
+ * fun test(m: ThrowingMap<String, MutableList<String>>) {
+ *   m["a"] += "b"
+ * }
+ * ```
+ * The above call is represented as a simple `KtFunctionCall` to `MutableList.plusAssign`, with the dispatch receiver referencing the
+ * `m["a"]`, which is again a simple `KtFunctionCall` to `ThrowingMap.get`.
+ */
+// TODO: make this not abstract
+public abstract class KtCompoundArrayAccessCall : KtCall(), KtCompoundAccessCall {
+    public abstract val indexArguments: List<KtExpression>
+
+    /**
+     * The `get` function that's invoked when reading values corresponding to the given [indexArguments].
+     */
+    public abstract val getSymbolAndReceiver: KtSymbolAndReceiver<KtFunctionSymbol>
+
+    /**
+     * The `set` function that's invoked when writing values corresponding to the given [indexArguments] and computed value from the
+     * operation.
+     */
+    public abstract val setSymbolAndReceiver: KtSymbolAndReceiver<KtFunctionSymbol>
+}
+
+/**
+ * The type of access to a variable or using the array access convention.
+ */
+public sealed class KtCompoundAccess {
+    /**
+     * The function that compute the value for this compound access. For example, if the access is `+=`, this is the resolved `plus`
+     * function. If the access is `++`, this is the resolved `inc` function.
+     */
+    public abstract val operationSymbolAndReceiver: KtSymbolAndReceiver<KtFunctionSymbol>
+
+    /**
+     * A compound access that read, compute, and write the computed value back. Note that calls to `<op>Assign` is not represented by this.
+     */
+    // TODO: make this not abstract
+    public abstract class CompoundAssign : KtCompoundAccess() {
+        public abstract val kind: KtCompoundAssignKind
+        public abstract val operand: KtExpression
+    }
+
+    /**
+     * A compound access that read, increment or decrement, and write the computed value back.
+     */
+    // TODO: make this not abstract
+    public abstract class IncOrDecOperation : KtCompoundAccess() {
+        public abstract val kind: KtIncOrDecOperationKind
+        public abstract val precedence: KtIncDecPrecedence
+    }
+}
+
+public enum class KtCompoundAssignKind {
+    PLUS_ASSIGN, MINUS_ASSIGN, MULTIPLY_ASSIGN, DIVIDE_ASSIGN, MODULO_ASSIGN
+}
+
+public enum class KtIncOrDecOperationKind {
+    INC, DEC
+}
+
+public enum class KtIncDecPrecedence {
+    PREFIX, POSTFIX
+}
+
+/**
+ * A receiver value of a call.
+ */
+public sealed class KtReceiverValue : ValidityTokenOwner
+
+/**
+ * A explicit receiver. For example
+ * ```
+ *   "".length // explicit receiver `""`
+ * ```
+ */
+public class KtExplicitReceiverValue(override val token: ValidityToken, private val _expression: KtExpression) : KtReceiverValue() {
+    public val expression: KtExpression get() = withValidityAssertion { _expression }
+}
+
+/**
+ * An implicit receiver. For example
+ * ```
+ * class A {
+ *   val i: Int = 1
+ *   fun test() {
+ *     i // implicit receiver bound to class `A`
+ *   }
  * }
  *
- * fun Int.invoke() {}
- */
-public class KtVariableWithInvokeFunctionCall(
-    private val _target: KtVariableLikeSymbol,
-    private val _argumentMapping: LinkedHashMap<KtExpression, KtValueParameterSymbol>,
-    private val _targetFunction: KtCallTarget,
-    private val _substitutor: KtSubstitutor,
-    override val token: ValidityToken
-) : KtDeclaredFunctionCall() {
-    public val target: KtVariableLikeSymbol get() = withValidityAssertion { _target }
-    override val argumentMapping: LinkedHashMap<KtExpression, KtValueParameterSymbol>
-        get() = withValidityAssertion { _argumentMapping }
-    override val targetFunction: KtCallTarget
-        get() = withValidityAssertion { _targetFunction }
-    override val substitutor: KtSubstitutor
-        get() = withValidityAssertion { _substitutor }
-}
-
-/**
- * Simple function call, e.g.,
- *
- * x.toString() // function call
- */
-public class KtFunctionCall(
-    private val _argumentMapping: LinkedHashMap<KtExpression, KtValueParameterSymbol>,
-    private val _targetFunction: KtCallTarget,
-    private val _substitutor: KtSubstitutor,
-    override val token: ValidityToken
-) : KtDeclaredFunctionCall() {
-    override val argumentMapping: LinkedHashMap<KtExpression, KtValueParameterSymbol>
-        get() = withValidityAssertion { _argumentMapping }
-    override val targetFunction: KtCallTarget
-        get() = withValidityAssertion { _targetFunction }
-    override val substitutor: KtSubstitutor
-        get() = withValidityAssertion { _substitutor }
-}
-
-/**
- * Annotation call, e.g.,
- *
- * @Retention(AnnotationRetention.SOURCE) // annotation call
- * annotation class Ann
- */
-public class KtAnnotationCall(
-    private val _argumentMapping: LinkedHashMap<KtExpression, KtValueParameterSymbol>,
-    private val _targetFunction: KtCallTarget,
-    override val token: ValidityToken
-) : KtDeclaredFunctionCall() {
-    override val argumentMapping: LinkedHashMap<KtExpression, KtValueParameterSymbol>
-        get() = withValidityAssertion { _argumentMapping }
-    override val targetFunction: KtCallTarget
-        get() = withValidityAssertion { _targetFunction }
-
-    // Type parameter is allowed for an annotation class but not allowed as members. So substitutor is probably never useful.
-    override val substitutor: KtSubstitutor = KtSubstitutor.Empty(token)
-}
-// TODO: Add other properties, e.g., useSiteTarget
-
-/**
- * Delegated constructor call, e.g.,
- *
- * open class A(a: Int)
- * class B(b: Int) : A(b) { // delegated constructor call (kind = SUPER_CALL)
- *   constructor() : this(1) // delegated constructor call (kind = THIS_CALL)
+ * fun String.test() {
+ *   length // implicit receiver bound to the `KtReceiverParameterSymbol` of type `String` declared by `test`.
  * }
+ * ```
  */
-public class KtDelegatedConstructorCall(
-    private val _argumentMapping: LinkedHashMap<KtExpression, KtValueParameterSymbol>,
-    private val _targetFunction: KtCallTarget,
-    public val kind: KtDelegatedConstructorCallKind,
+public class KtImplicitReceiverValue(override val token: ValidityToken, private val _boundSymbol: KtSymbol) : KtReceiverValue() {
+    public val boundSymbol: KtSymbol get() = withValidityAssertion { _boundSymbol }
+}
+
+/**
+ * A smart-casted receiver. For example
+ * ```
+ * fun Any.test() {
+ *   if (this is String) {
+ *     length // smart-casted implicit receiver bound to the `KtReceiverParameterSymbol` of type `String` declared by `test`.
+ *   }
+ * }
+ * ```
+ */
+public class KtSmartCastedReceiverValue(private val _original: KtReceiverValue, private val _smartCastType: KtType) : KtReceiverValue() {
     override val token: ValidityToken
-) : KtDeclaredFunctionCall() {
-    override val argumentMapping: LinkedHashMap<KtExpression, KtValueParameterSymbol>
-        get() = withValidityAssertion { _argumentMapping }
-    override val targetFunction: KtCallTarget
-        get() = withValidityAssertion { _targetFunction }
-
-    // A delegate constructor call never has any type argument.
-    override val substitutor: KtSubstitutor = KtSubstitutor.Empty(token)
-}
-
-public enum class KtDelegatedConstructorCallKind { SUPER_CALL, THIS_CALL }
-
-/**
- * Represents function(s) in which call was resolved,
- * Can be success [KtSuccessCallTarget] in this case there only one such function
- * Or erroneous [KtErrorCallTarget] in this case there can be any count of candidates
- */
-public sealed class KtCallTarget : ValidityTokenOwner {
-    public abstract val candidates: Collection<KtFunctionLikeSymbol>
-}
-
-/**
- * Success call of [symbol]
- */
-public class KtSuccessCallTarget(private val _symbol: KtFunctionLikeSymbol, override val token: ValidityToken) : KtCallTarget() {
-    public val symbol: KtFunctionLikeSymbol get() = withValidityAssertion { _symbol }
-    override val candidates: Collection<KtFunctionLikeSymbol> get() = withValidityAssertion { listOf(symbol) }
-}
-
-/**
- * Function call with errors, possible candidates are [candidates]
- */
-public class KtErrorCallTarget(
-    private val _candidates: Collection<KtFunctionLikeSymbol>,
-    private val _diagnostic: KtDiagnostic,
-    override val token: ValidityToken
-) : KtCallTarget() {
-    public val diagnostic: KtDiagnostic get() = withValidityAssertion { _diagnostic }
-    override val candidates: Collection<KtFunctionLikeSymbol> get() = withValidityAssertion { _candidates }
+        get() = _original.token
+    public val original: KtReceiverValue get() = withValidityAssertion { _original }
+    public val smartCastType: KtType get() = withValidityAssertion { _smartCastType }
 }
