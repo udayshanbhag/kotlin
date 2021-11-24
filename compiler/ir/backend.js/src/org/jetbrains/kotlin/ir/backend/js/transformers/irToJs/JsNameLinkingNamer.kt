a1b8ffe37f614da5a4891358d4bd1f01d300f3d9
Anton Bannykh <anton.bannykh@jetbrains.com>
2021-11-24 18:35:59 +0300

    [JS IR] switch to the IC Ir2Js and fix tests

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/transformers/irToJs/JsNameLinkingNamer.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/transformers/irToJs/JsNameLinkingNamer.kt
@@ -12,7 +12,6 @@ import org.jetbrains.kotlin.ir.symbols.IrClassSymbol
 import org.jetbrains.kotlin.ir.types.IrSimpleType
 import org.jetbrains.kotlin.ir.util.isEffectivelyExternal
 import org.jetbrains.kotlin.ir.util.parentAsClass
-import org.jetbrains.kotlin.ir.util.render
 import org.jetbrains.kotlin.js.backend.ast.*
 import org.jetbrains.kotlin.utils.DFS
 import org.jetbrains.kotlin.utils.addToStdlib.safeAs
@@ -34,7 +33,7 @@ class JsNameLinkingNamer(private val context: JsIrBackendContext) : IrNamerBase(
             val jsModule: String? = declaration.getJsModule()
             val maybeParentFile: IrFile? = declaration.parent as? IrFile
             val fileJsModule: String? = maybeParentFile?.getJsModule()
-            val jsQualifier: String? = maybeParentFile?.getJsQualifier()
+            val jsQualifier: List<JsName>? = maybeParentFile?.getJsQualifier()?.split('.')?.map { JsName(it, false) }
 
             when {
                 jsModule != null -> {
@@ -45,19 +44,22 @@ class JsNameLinkingNamer(private val context: JsIrBackendContext) : IrNamerBase(
 
                 fileJsModule != null -> {
                     if (declaration !in nameMap) {
-                        val moduleName = JsName("\$module\$$jsModule", true)
+                        val moduleName = JsName(sanitizeName("\$module\$$fileJsModule"), true)
                         importedModules += JsImportedModule(fileJsModule, moduleName, null)
                         val qualifiedReference =
-                            if (jsQualifier == null) moduleName.makeRef() else JsNameRef(jsQualifier, moduleName.makeRef())
-                        imports[declaration] = JsNameRef(declaration.getJsNameOrKotlinName().identifier, qualifiedReference)
+                            if (jsQualifier == null) moduleName.makeRef() else (listOf(moduleName) + jsQualifier).makeRef()
+                        imports[declaration] = jsElementAccess(declaration.getJsNameOrKotlinName().identifier, qualifiedReference)
                         return declaration.getName()
                     }
                 }
 
                 else -> {
-                    var name = declaration.getJsNameOrKotlinName().identifier
-                    if (jsQualifier != null)
-                        name = "$jsQualifier.$name"
+                    val name = declaration.getJsNameOrKotlinName().identifier
+
+                    if (jsQualifier != null) {
+                        imports[declaration] = jsElementAccess(name, jsQualifier.makeRef())
+                        return declaration.getName()
+                    }
 
                     return name.toJsName(temporary = false)
                 }
@@ -118,4 +120,12 @@ private fun IrField.safeName(): String {
     return sanitizeName(name.asString()).let {
         if (it.lastOrNull()!!.isDigit()) it + "_" else it // Avoid name clashes
     }
+}
+
+private fun List<JsName>.makeRef(): JsNameRef {
+    var result = this[0].makeRef()
+    for (i in 1 until this.size) {
+        result = JsNameRef(this[i], result)
+    }
+    return result
 }
